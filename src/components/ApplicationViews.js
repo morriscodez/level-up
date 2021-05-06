efca467a379da893a804f2ddfcc45883c31e1cde
Dylan Morris <dylanrobertmorris@gmail.com>
2021-05-06 09:55:39 -0500

    build: app views

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,13 +1,20 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { GameList } from "./game/GameList"
+import { GameProvider } from "./game/GameProvider"
 
 export const ApplicationViews = () => {
     return <>
         <main style={{
             margin: "5rem 2rem",
-            backgroundColor: "lightgoldenrodyellow"
+            backgroundColor: "lightgoldenrodyellow",
+            lineHeight: "1.75rem"
         }}>
-            Application views
+            <GameProvider>
+                <Route exact path ='/'>
+                    <GameList />
+                </Route>
+            </GameProvider>
         </main>
     </>
 }
