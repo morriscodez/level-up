9202d951fe6bec74427055228ae58562d27a544b
Dylan Morris <dylanrobertmorris@gmail.com>
2021-05-06 10:37:20 -0500

    feat: games displayed on homepage

--- src/components/game/GameProvider.js
+++ src/components/game/GameProvider.js
@@ -6,15 +6,17 @@ export const GameProvider = (props) => {
     const [ games, setGames ] = useState([])
 
     const getGames = () => {
-        return fetch("http://localhost:8088/games", {
+        return fetch("http://localhost:8000/games", {
             headers:{
                 "Authorization": `Token ${localStorage.getItem("lu_token")}`
             }
         })
+        .then(response => response.json())
+        .then(setGames)
     }
 
     return (
-        <GameContext.Provider value={{ games, GetGames }} >
+        <GameContext.Provider value={{ games, getGames }} >
             { props.children }
         </GameContext.Provider>
     )
