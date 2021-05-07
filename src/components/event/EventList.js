304c19f7bea209f3780d25069f0e5c68abc95eae
Dylan Morris <dylanrobertmorris@gmail.com>
2021-05-07 11:04:57 -0500

    build: refactored event list

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -9,20 +9,36 @@ export const EventList = (props) => {
     }, [])
 
     return (
-        <article className="events">
-            <hr></hr>
-            {
-                events.map(event => {
-                    return <section key={`event--${event.id}`} className="event">
-                        <div className="event__name">Event name: {event.name}</div>
-                        <div className="event__description">Description: {event.description} players needed</div>
-                        <div className="event__date">Event Happening on: {event.date}</div>
-                        <div className="event__address">Event Happening at: {event.address}</div>
-                        <div className="event__address">Game We're Playing: {event.game?.label}</div>
-                    </section>
-                })
-            }
-        </article>
+        <>
+            <article className="events">
+                <hr></hr>
+                <header className="events__header">
+                    <h1>Level Up Game Events</h1>
+                </header>
+                {
+                    events.map(event => {
+                        return <section key={`event--${event.id}`} className="registration">
+                                <div className="event__name">Event name: {event.name}</div>
+                            <div className="registration_game">{event.game?.label}</div>
+                            <div className="event__description">Description: {event.description} players needed</div>
+                            <div>
+                                {
+                                    new Date(event.date).toLocaleDateString("en-US", {
+                                        weekday: 'long',
+                                        year: 'numeric',
+                                        month: 'long',
+                                        day: 'numeric'
+                                    })
+                                }
+                                @ {event.time}
+
+                            </div>
+                            <div className="event__address">Event Happening at: {event.address}</div>
+                        </section>
+                    })
+                }
+            </article>
+        </>
     )
 
 }
