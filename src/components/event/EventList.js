d362bc02f48b40ab91bbacc9c04c916e4acd38f5
Dylan Morris <dylanrobertmorris@gmail.com>
2021-05-10 10:20:35 -0500

    build: added join event button to events list

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,13 +1,16 @@
 import React, { useContext, useEffect } from "react"
 import { useHistory } from "react-router"
 import { EventContext } from "./EventProvider.js"
+import "./Events.css"
 
 export const EventList = (props) => {
-    const { events, getEvents } = useContext(EventContext)
+    const { events, getEvents, joinEvent } = useContext(EventContext)
     const history = useHistory()
+    
     useEffect(() => {
         getEvents()
     }, [])
+
 
     return (
         <>
@@ -26,22 +29,25 @@ export const EventList = (props) => {
                     events.map(event => {
                         return <section key={`event--${event.id}`} className="registration">
                                 <div className="event__name">Event name: {event.name}</div>
-                            <div className="registration_game">{event.game?.label}</div>
-                            <div className="event__description">Description: {event.description} players needed</div>
-                            <div>
-                                {
-                                    new Date(event.date).toLocaleDateString("en-US", {
+                                    <div className="registration_game">{event.game?.label}</div>
+                                    <div className="event__description">Description: {event.description} players needed</div>
+                                    <div>
+                                    {
+                                        new Date(event.date).toLocaleDateString("en-US", {
                                         weekday: 'long',
                                         year: 'numeric',
                                         month: 'long',
                                         day: 'numeric'
                                     })
-                                }
+                                    }
                                 @ {event.time}
 
-                            </div>
-                            <div className="event__address">Event Happening at: {event.address}</div>
-                        </section>
+                                </div>
+                                <div className="event__address">Event Happening at: {event.address}</div>
+                                    <button className="btn btn-2" onClick={() => joinEvent(event.id)}>
+
+                                    </button>
+                                </section>
                     })
                 }
             </article>
