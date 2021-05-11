c7460fa7577a1b0d0e9e87a121bdf0e2f0d208cc
Dylan Morris <dylanrobertmorris@gmail.com>
2021-05-11 16:04:42 -0500

    feat: games can be edited from form

--- src/components/game/GameList.js
+++ src/components/game/GameList.js
@@ -22,6 +22,12 @@ export const GameList = (props) => {
             {
                 games.map(game => {
                     return <section key={`game--${game.id}`} className="game">
+                        <div className="game__edit">
+                            <button className="btn btn-3" onClick={e => history.push(`/games/${game.id}/edit`) }>
+                                Edit
+                            </button>
+
+                        </div>
                         <div className="game__title">{game.label}</div>
                         <div className="game__players">{game.number_of_players} players needed</div>
                         <div className="game__skillLevel">Skill level is {game.skill_level}</div>
